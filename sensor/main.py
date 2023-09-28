b91a049a4b22b44337035ce33ddbaf1cbba4e711
Nasjk <123550347+Nasjk@users.noreply.github.com>
2023-09-28 10:21:17 +0200

    Update main.py

--- sensor/main.py
+++ sensor/main.py
@@ -8,7 +8,7 @@ import json
 temp_config = open('config.json')
 config = json.load(temp_config)
 
-# Ange vilket GPIO-stift sensorn är ansluten till
+# Ange vilket GPIO-stift sensorn är ansluten till 
 dat = machine.Pin(config['pin'])
 
 # Skapa en onewire-objekt
@@ -24,18 +24,16 @@ unit_id_str = '(B\x05\xefO \x01\xf5'
 # Konvertera enhets-ID från sträng till en bytearray
 byte_array = bytearray(unit_id_str, 'utf-8')
 
-roms2 = hex(int.from_bytes(roms[0], 'little'))
-
 # Konvertera bytearray till hexadecimal representation
-hexadecimal_representation = ''.join(['{:02x}'.format(byte) for byte in byte_array])
+hex_id = ''.join(['{:02x}'.format(byte) for byte in byte_array])
 
-
-# Loopa och skriv ut temperaturen, unit_ID och sensor_ID 
+# Loopa och skriv ut temperaturen, unit_ID och sensor_ID i all evighet
 while True:
-    print('', hexadecimal_representation, roms2[2:], end='')
     ds.convert_temp()
     time.sleep_ms(config['interval'])
     for rom in roms:
+        roms2 = hex(int.from_bytes(rom, 'little'))
+        print(hex_id, roms2[2:], end='')
         print('', ds.read_temp(rom), end=' ')
     print()
 
